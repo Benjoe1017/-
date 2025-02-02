2fc441cdc89d5ac46479b302c6ee0cfb02f840e3
Chiu Kuan Hsun <108409184+ChiuKuanHsun@users.noreply.github.com>
2025-02-02 15:07:24 +0800

    Update popup.js

--- 學習歷程登入優化V2.5/popup.js
+++ 學習歷程登入優化V2.5/popup.js
@@ -12,9 +12,7 @@ document.getElementById('Setting').addEventListener('click', function() {
     window.location.href="setting.html";
 });
 function createNotification(message) {
-    // 創建一個新的 div 元素
     var notification = document.createElement('div');
-    // 設置提示元素的樣式
     notification.style.position = 'fixed';
     notification.style.top = '50%';
     notification.style.left = '50%';
@@ -24,14 +22,11 @@ function createNotification(message) {
     notification.style.border = '1px solid #000000';
     notification.style.boxShadow = '0 2px 5px rgba(0, 0, 0, 0.2)';
     notification.style.zIndex = '9999';
-    // 設置提示元素的內容
     notification.textContent = message;
-    // 將提示元素添加到 body 元素中
     document.body.appendChild(notification);
-    // 在一定時間後移除提示元素
     setTimeout(function() {
         document.body.removeChild(notification);
-    }, 1750); // 3 秒後移除提示元素
+    }, 1750); 
 }
 
 
